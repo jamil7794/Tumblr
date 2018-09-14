d95f0268aa01c7204b3516dba57f928fd2b539f7
JamilJalal <jamil.jalal94@gmail.com>
2018-09-14 16:06:13 -0400

    Done

---
+++ Tumblr/Tumblr/imgCell.swift
@@ -0,0 +1,27 @@
+//
+//  imgCell.swift
+//  Tumblr
+//
+//  Created by Jamil Jalal on 9/14/18.
+//  Copyright © 2018 Jamil Jalal. All rights reserved.
+//
+
+import UIKit
+
+class imgCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
