4ba15c04195f707a7754590021521ced7b06f3af
JamilJalal <jamil.jalal94@gmail.com>
2018-09-15 15:28:20 -0400

    Gif Uploaded

--- Tumblr/Tumblr/ViewController.swift
+++ Tumblr/Tumblr/ViewController.swift
@@ -15,40 +15,49 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var urlImg: [[String: Any]] = []
     var posts: [[String: Any]] = []
-   // var imageLink = [String]()
-    
+    let alertController = UIAlertController(title: "Error", message: "Connect to the Internet", preferredStyle: .alert)
     var refreshControl: UIRefreshControl!
-    
  
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.activityIndicator.startAnimating()
         tableView.dataSource = self
         tableView.delegate = self
+        let tryAgain = UIAlertAction(title: "Try Again", style: .default) { (action) in
+            self.fetchPic()
+        }
+        alertController.addAction(tryAgain)
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(ViewController.didPullToRefresh), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         fetchPic()
     }
     
+    
     @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
         fetchPic()
     }
 
     func fetchPic(){
+        
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
         let task = session.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 print(error.localizedDescription)
+                UIApplication.shared.keyWindow?.rootViewController?.present(self.alertController, animated: true){}
             } else if let data = data,
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
                 // Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
-                self.tableView.reloadData()
                 self.refreshControl.endRefreshing()
+                self.tableView.reloadData()
+                self.activityIndicator.stopAnimating()
+                self.activityIndicator.isHidden = true
             }
         }
         task.resume()
