ca07d4f08ba9cf9f3ad85a2bcb199d3974682f70
JamilJalal <jamil.jalal94@gmail.com>
2018-09-20 11:42:01 -0400

    Added zoomable pic

--- Tumblr/Tumblr/FullScreenPhotoVC.swift
+++ Tumblr/Tumblr/FullScreenPhotoVC.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class FullScreenPhotoVC: UIViewController {
+class FullScreenPhotoVC: UIViewController, UIScrollViewDelegate {
 
     var pic : [String: Any]!
     
-    @IBOutlet weak var fullScreenPic: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if let photos = pic["photos"] as? [[String: Any]] {
@@ -25,13 +28,20 @@ class FullScreenPhotoVC: UIViewController {
             let urlString = originalSize["url"] as! String
             // 4.
             let url = URL(string: urlString)
-            fullScreenPic.af_setImage(withURL: url!)
+            imageView.af_setImage(withURL: url!)
        }
+        scrollView.contentSize = imageView.image!.size
+        scrollView.delegate = self
         
     }
     
     @IBAction func closeBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
     
 }
